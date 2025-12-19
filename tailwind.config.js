01bd9b90e9488d1fdcdc1b3e7fbbea94d37c4f53
Dannyb685 <60555926+Dannyb685@users.noreply.github.com>
2025-12-19 11:42:41 -0800

    Fix CSS leakage by disabling preflight and scoping styles to .opioid-tool-container

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,6 +3,10 @@ module.exports = {
     content: [
         "./src/**/*.{js,ts,jsx,tsx}",
     ],
+    important: '.opioid-tool-container',
+    corePlugins: {
+        preflight: false,
+    },
     theme: {
         extend: {},
     },
